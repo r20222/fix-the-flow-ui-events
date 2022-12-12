7e818c66903832400077404e6ec9b57c6f551aa0
r20222 <101579892+r20222@users.noreply.github.com>
2022-12-12 17:40:55 +0100

    button styles javascript clickss

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,87 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+
+// eerste button
+let interactionRotation = document.querySelector('a:nth-of-type(1)')
+interactionRotation.addEventListener('click', rotationRound)
+interactionRotation.addEventListener('animationend', rotationRound)
+function rotationRound(){
+  interactionRotation.classList.toggle('rotation')
+}
+
+// tweede button
+let interactionCollide = document.querySelector('a:nth-of-type(2)')
+interactionCollide.addEventListener('animationend', collideButton)
+interactionCollide.addEventListener('click', collideButton)
+
+function collideButton(){
+  interactionCollide.classList.toggle('collide')
+}
+
+// derde button
+let interactionFootbal = document.querySelector('a:nth-of-type(3)')
+interactionFootbal.addEventListener('click', footbalButton)
+interactionFootbal.addEventListener('animationend', footbalButton)
+
+function footbalButton(){
+  interactionFootbal.classList.toggle('footbal')
+}
+
+// vierde button
+let interactionGreen = document.querySelector('a:nth-of-type(4)')
+interactionGreen.addEventListener('dblclick', greenColor)
+interactionGreen.addEventListener('animationend', greenColor)
+
+function greenColor(){
+  interactionGreen.classList.toggle('green')
+}
+
+// vijfde button
+
+let interactionBigger = document.querySelector('a:nth-of-type(5)')
+interactionBigger.addEventListener('click', bigButton)
+interactionBigger.addEventListener('animationend', bigButton)
+
+function bigButton(){
+  interactionBigger.classList.toggle('bigger')
+}
+
+// zesde button
+
+let interactionLonger = document.querySelector('a:nth-of-type(6)')
+interactionLonger.addEventListener('click', longButton)
+interactionLonger.addEventListener('animationend', longButton)
+
+function longButton(){
+  interactionLonger.classList.toggle('longer')
+}
+
+// zevende button
+
+let interactionFlip = document.querySelector('a:nth-of-type(7)')
+interactionFlip.addEventListener('click', flip)
+interactionFlip.addEventListener('animationend', flip)
+
+function flip(){
+  interactionFlip.classList.toggle('flip')
+}
+
+// achtste button
+
+let interactionHanger = document.querySelector('a:nth-of-type(8)')
+interactionHanger.addEventListener('click', buttonHang)
+interactionHanger.addEventListener('animationend', buttonHang)
+
+function buttonHang(){
+  interactionHanger.classList.toggle('hanger')
+}
+
+// negende button
+let interactionFalling = document.querySelector('a:nth-of-type(9)')
+interactionFalling.addEventListener('click', falling)
+
+function falling(){
+  interactionFalling.classList.toggle('falling')
+}
